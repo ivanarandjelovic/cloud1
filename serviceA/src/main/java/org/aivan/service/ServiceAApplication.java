bb7ecd18e3bb09cd8be33f17748669773a898286
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2018-08-24 09:21:05 +0200

    Add inter-service calls

--- serviceA/src/main/java/org/aivan/service/ServiceAApplication.java
+++ serviceA/src/main/java/org/aivan/service/ServiceAApplication.java
@@ -2,11 +2,14 @@ package org.aivan.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +20,12 @@ public class ServiceAApplication {
 	@Value("${spring.application.name}")
 	String appName;
 
+	@Autowired
+	RestTemplateBuilder restTemplateBuilder;
+	
+	@Autowired
+	ServiceAConfiguration configuration;
+	
 	public static void main(String[] args) {
 		log.info("Application starting ...");
 		SpringApplication.run(ServiceAApplication.class, args);
@@ -25,6 +34,10 @@ public class ServiceAApplication {
 	@RequestMapping("/")
 	public String serviceA() {
 		log.info("serviceA method called!");
-		return "Hello from " + appName + "!";
+		
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		String response = restTemplate.getForObject(configuration.getService1Url(), String.class);
+		
+		return "Hello from " + appName + "! I called other service and got: "+response;
 	}
 }
---
+++ serviceA/src/main/java/org/aivan/service/ServiceAConfiguration.java
@@ -0,0 +1,20 @@
+package org.aivan.service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("service-a")
+public class ServiceAConfiguration {
+
+	String service1Url;
+
+	public String getService1Url() {
+		return service1Url;
+	}
+
+	public void setService1Url(String service1Url) {
+		this.service1Url = service1Url;
+	}
+
+}
