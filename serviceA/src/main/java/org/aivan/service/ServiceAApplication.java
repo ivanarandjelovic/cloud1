7d9d4243a7fe8f80058af86c7d046123b80e1f8a
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2018-08-24 14:51:47 +0200

    Add kubernetes deployments and services

--- serviceA/src/main/java/org/aivan/service/ServiceAApplication.java
+++ serviceA/src/main/java/org/aivan/service/ServiceAApplication.java
@@ -1,5 +1,8 @@
 package org.aivan.service;
 
+import java.net.URI;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +10,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ServiceAApplication {
+
+	private static final String SERVICE1_NAME = "service1";
 
 	static Log log = LogFactory.getLog(ServiceAApplication.class);
 
@@ -24,7 +33,7 @@ public class ServiceAApplication {
 	RestTemplateBuilder restTemplateBuilder;
 	
 	@Autowired
-	ServiceAConfiguration configuration;
+	private DiscoveryClient discoveryClient;
 	
 	public static void main(String[] args) {
 		log.info("Application starting ...");
@@ -35,8 +44,18 @@ public class ServiceAApplication {
 	public String serviceA() {
 		log.info("serviceA method called!");
 		
+		List<ServiceInstance> services = discoveryClient.getInstances(SERVICE1_NAME);
+		if(services.size() == 0) {
+			log.error("Service1 is not available1");
+			return "ERROR: No services1 available!";
+		}
+		
+		ServiceInstance service1 = services.get(0);
+		URI service1Url = service1.getUri();
+		log.debug("service1 url is: "+service1Url.toString());
+		
 		RestTemplate restTemplate = restTemplateBuilder.build();
-		String response = restTemplate.getForObject(configuration.getService1Url(), String.class);
+		String response = restTemplate.getForObject(service1Url, String.class);
 		
 		return "Hello from " + appName + "! I called other service and got: "+response;
 	}
