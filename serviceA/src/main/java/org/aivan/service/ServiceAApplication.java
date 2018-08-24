5971dcdd7894d32795024c85bd53f9c8445102e6
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2018-08-24 15:22:50 +0200

    No need for discovery client

--- serviceA/src/main/java/org/aivan/service/ServiceAApplication.java
+++ serviceA/src/main/java/org/aivan/service/ServiceAApplication.java
@@ -1,8 +1,5 @@
 package org.aivan.service;
 
-import java.net.URI;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 public class ServiceAApplication {
 
-	private static final String SERVICE1_NAME = "service1";
+//	private static final String SERVICE1_NAME = "service1";
 
 	static Log log = LogFactory.getLog(ServiceAApplication.class);
 
@@ -31,9 +26,6 @@ public class ServiceAApplication {
 
 	@Autowired
 	RestTemplateBuilder restTemplateBuilder;
-	
-	@Autowired
-	private DiscoveryClient discoveryClient;
 	
 	public static void main(String[] args) {
 		log.info("Application starting ...");
@@ -44,18 +36,24 @@ public class ServiceAApplication {
 	public String serviceA() {
 		log.info("serviceA method called!");
 		
-		List<ServiceInstance> services = discoveryClient.getInstances(SERVICE1_NAME);
-		if(services.size() == 0) {
-			log.error("Service1 is not available1");
-			return "ERROR: No services1 available!";
-		}
-		
-		ServiceInstance service1 = services.get(0);
-		URI service1Url = service1.getUri();
-		log.debug("service1 url is: "+service1Url.toString());
+//		List<ServiceInstance> services = discoveryClient.getInstances(SERVICE1_NAME);
+//		if(services.size() == 0) {
+//			log.error("Service1 is not available1");
+//			return "ERROR: No services1 available!";
+//		} else {
+//			log.info("There are "+services.size()+" services");
+//			for(ServiceInstance si:services) {
+//				log.info("instance = "+si.toString());
+//			}
+//		}
+//		
+//		ServiceInstance service1 = services.get(0);
+//		URI service1Url = service1.getUri();
+//		log.debug("service1 url is: "+service1Url.toString());
 		
 		RestTemplate restTemplate = restTemplateBuilder.build();
-		String response = restTemplate.getForObject(service1Url, String.class);
+		log.warn("using fixed service name1");
+		String response = restTemplate.getForObject("http://service1:8081/", String.class);
 		
 		return "Hello from " + appName + "! I called other service and got: "+response;
 	}
