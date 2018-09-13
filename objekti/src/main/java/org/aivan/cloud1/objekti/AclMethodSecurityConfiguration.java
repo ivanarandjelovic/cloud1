61ebb4f4cd2e468df7edf80bcf287b9d839a394a
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2018-09-13 17:46:55 +0200

    Pining away ...

--- objekti/src/main/java/org/aivan/cloud1/objekti/AclMethodSecurityConfiguration.java
+++ objekti/src/main/java/org/aivan/cloud1/objekti/AclMethodSecurityConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.cache.ehcache.EhCacheFactoryBean;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.acls.AclPermissionEvaluator;
@@ -16,8 +18,10 @@ import org.springframework.security.acls.domain.ConsoleAuditLogger;
 import org.springframework.security.acls.domain.DefaultPermissionGrantingStrategy;
 import org.springframework.security.acls.domain.EhCacheBasedAclCache;
 import org.springframework.security.acls.jdbc.BasicLookupStrategy;
+import org.springframework.security.acls.jdbc.JdbcAclService;
 import org.springframework.security.acls.jdbc.JdbcMutableAclService;
 import org.springframework.security.acls.jdbc.LookupStrategy;
+import org.springframework.security.acls.model.AclService;
 import org.springframework.security.acls.model.PermissionGrantingStrategy;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
@@ -27,14 +31,85 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class AclMethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
 
-  @Autowired
-  MethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler;
+	@Autowired
+	MethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler;
+	
+	@Autowired
+	void setMethodSecurityExpressionHandler(MethodSecurityExpressionHandler handler) {
+		System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+		System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+		System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+		System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+		System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+	}
+	
+//
+//	@Override
+//	protected MethodSecurityExpressionHandler createExpressionHandler() {
+//		return defaultMethodSecurityExpressionHandler;
+//	}
+//
+	@Bean
+	public MethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler() {
+		DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+		AclPermissionEvaluator permissionEvaluator = new AclPermissionEvaluator(getAclService());
+		expressionHandler.setPermissionEvaluator(permissionEvaluator);
+		return expressionHandler;
+	}
 
-  @Override
-  protected MethodSecurityExpressionHandler createExpressionHandler() {
-    return defaultMethodSecurityExpressionHandler;
-  }
-  
+	@Bean
+	public PermissionEvaluator createPermissionEvaluator() {
+		System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		return new AclPermissionEvaluator(getAclService());
+	}
 
-  
+	@Autowired
+	DataSource dataSource;
+
+	@Bean
+	public AclService getAclService() {
+		return new JdbcMutableAclService(dataSource, lookupStrategy(), aclCache());
+	}
+
+	@Bean
+	public EhCacheBasedAclCache aclCache() {
+		return new EhCacheBasedAclCache(aclEhCacheFactoryBean().getObject(), permissionGrantingStrategy(),
+				aclAuthorizationStrategy());
+	}
+
+	@Bean
+	public EhCacheFactoryBean aclEhCacheFactoryBean() {
+		EhCacheFactoryBean ehCacheFactoryBean = new EhCacheFactoryBean();
+		ehCacheFactoryBean.setCacheManager(aclCacheManager().getObject());
+		ehCacheFactoryBean.setCacheName("aclCache");
+		return ehCacheFactoryBean;
+	}
+
+	@Bean
+	public EhCacheManagerFactoryBean aclCacheManager() {
+		return new EhCacheManagerFactoryBean();
+	}
+
+	@Bean
+	public LookupStrategy lookupStrategy() {
+		return new BasicLookupStrategy(dataSource, aclCache(), aclAuthorizationStrategy(), new ConsoleAuditLogger());
+	}
+
+	@Bean
+	public AclAuthorizationStrategy aclAuthorizationStrategy() {
+		return new AclAuthorizationStrategyImpl(new SimpleGrantedAuthority("ROLE_ADMIN"));
+	}
+
+	@Bean
+	public PermissionGrantingStrategy permissionGrantingStrategy() {
+		return new DefaultPermissionGrantingStrategy(new ConsoleAuditLogger());
+	}
+
 }
